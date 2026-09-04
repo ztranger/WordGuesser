1f39a45135bc632ff4227db41c12929eab7053bf
Kirill <altunin.k@topapp.games>
2026-09-04 14:57:38 +0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hpg.wordguesser"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
